6607bb07fbd8de818155fd39b1235b5d2019ad06
amu <carvin_lo@163.com>
2019-06-15 17:44:58 +0800

    接口指向本地，样式资源本地化

--- src/common/config.js
+++ src/common/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://conduit.productionready.io/api";
+// export const API_URL = "https://conduit.productionready.io/api";
+export const API_URL = "http://127.0.0.1:3000/api";
 export default API_URL;
